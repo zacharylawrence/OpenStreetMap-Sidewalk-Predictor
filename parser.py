24c0c86e8ee3b5c93d6bc5118f56b13ed8d539a9
Zachary Lawrence <zacharyclawrence@gmail.com>
2015-02-27 14:57:30 -0500

    Basic setup of parsing osm file

---
+++ parser.py
@@ -0,0 +1,28 @@
+from imposm.parser import OSMParser
+
+# simple class that handles the parsed OSM data.
+class HighwayCounter(object):
+    coords = {};
+    highways = {};
+
+    def ways(self, ways):
+        # callback method for ways
+        for osmid, tags, refs in ways:
+            # Only allow 'secondary' and 'residential' highways to have assumed sidewalks
+            if 'highway' in tags and (tags['highway'] == 'secondary' or tags['highway'] == 'residential'):
+                self.highways[osmid] = refs
+
+    def coords_callback(self, coords):
+        for (id, lat, long) in coords:
+            self.coords[id] = (lat, long)
+
+# instantiate counter and parser and start parsing
+counter = HighwayCounter()
+p = OSMParser(concurrency=4, ways_callback=counter.ways, coords_callback=counter.coords_callback)
+p.parse('map.osm')
+
+# Finished parsing, now use the data
+for osmid in counter.highways:          # Way ids
+    for id in counter.highways[osmid]:   # Coord ids
+        print counter.coords[id]
+    print
