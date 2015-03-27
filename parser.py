7ae3b4be525bc158808c9c96980ceed857f31d9f
Zachary Lawrence <zacharyclawrence@gmail.com>
2015-03-27 16:14:53 -0400

    Working on locating intersections and labeling them as crosswalks instead of sidewalks

--- parser.py
+++ parser.py
@@ -4,20 +4,28 @@ import math
 
 # simple class that handles the parsed OSM data.
 class HighwayCounter(object):
-    coords = {};
-    highways = {};
+    coords = {}
+    highways = {}
+    intersections = set()
 
     def ways(self, ways):
-        # callback method for ways
         for osmid, tags, refs in ways:
             # Only allow 'secondary', 'residential' and 'tertiary' highways to have assumed sidewalks
-            valid_highways = set(['secondary', 'residential', 'tertiary'])
+            valid_highways = set(['secondary', 'residential', 'tertiary', 'residential'])
             if 'highway' in tags and tags['highway'] in valid_highways:
                 self.highways[osmid] = refs
 
     def coords_callback(self, coords):
         for (id, long, lat) in coords:  # Be careful of the order of lat/long -> OSM uses long/lat
             self.coords[id] = (lat, long)
+
+    def calculateIntersections(self):
+        seen = set()
+        for way, refs in self.highways.items():
+            for ref in refs:
+                if ref in seen:
+                    self.intersections.add(ref)
+                seen.add(ref)
 
 # Helper sliding window iterater method
 # See: http://stackoverflow.com/questions/6822725/rolling-or-sliding-window-iterator-in-python
@@ -44,6 +52,12 @@ def window(seq, n=2):
 counter = HighwayCounter()
 p = OSMParser(concurrency=4, ways_callback=counter.ways, coords_callback=counter.coords_callback)
 p.parse('map2.osm')
+counter.calculateIntersections()
+
+print "Intersections:"
+for id in counter.intersections:
+	print(counter.coords[id])
+print
 
 # Finished parsing, now use the data
 for osmid in counter.highways:                  # Way ids
@@ -51,32 +65,34 @@ for osmid in counter.highways:                  # Way ids
     gen = window(counter.highways[osmid], 3)    # Coord ids (in order)
     for ids in gen:
         prev_id, id, next_id = ids
-        prev_lat, prev_long = counter.coords[prev_id]
-        lat, long = counter.coords[id]
-        next_lat, next_long = counter.coords[next_id]
 
-        in_angle = math.atan2(lat - prev_lat, long - prev_long)
-        out_angle = math.atan2(next_lat - lat, next_long - long)
+        if id not in counter.intersections:
+            prev_lat, prev_long = counter.coords[prev_id]
+            lat, long = counter.coords[id]
+            next_lat, next_long = counter.coords[next_id]
 
-        average_x = (math.sin(in_angle) + math.sin(out_angle)) / 2
-        average_y = (math.cos(in_angle) + math.cos(out_angle)) / 2
-        average_angle = math.atan2(average_x, average_y)
+            in_angle = math.atan2(lat - prev_lat, long - prev_long)
+            out_angle = math.atan2(next_lat - lat, next_long - long)
 
-        perpendicular_angle = math.pi / 2 + average_angle
+            average_x = (math.sin(in_angle) + math.sin(out_angle)) / 2
+            average_y = (math.cos(in_angle) + math.cos(out_angle)) / 2
+            average_angle = math.atan2(average_x, average_y)
 
-        # Here we are assuming that things are on a flat plane, not mapped to a globe!
-        sidewalk_distance = 5 # In meters - Not currently used, just trial-and-errored 0.00005
-        delta_lat = math.sin(perpendicular_angle) * 0.00005
-        delta_long = math.cos(perpendicular_angle) * 0.00005
+            perpendicular_angle = math.pi / 2 + average_angle
 
-        sidewalk_lat = lat + delta_lat
-        sidewalk_long = long + delta_long
+            # Here we are assuming that things are on a flat plane, not mapped to a globe!
+            sidewalk_distance = 5 # In meters - Not currently used, just trial-and-errored 0.00005
+            delta_lat = math.sin(perpendicular_angle) * 0.00005
+            delta_long = math.cos(perpendicular_angle) * 0.00005
 
-        print str(sidewalk_lat) + ',' + str(sidewalk_long)
+            sidewalk_lat = lat + delta_lat
+            sidewalk_long = long + delta_long
 
-        sidewalk_lat = lat - delta_lat
-        sidewalk_long = long - delta_long
+            print str(sidewalk_lat) + ',' + str(sidewalk_long)
 
-        print str(sidewalk_lat) + ',' + str(sidewalk_long)
+            sidewalk_lat = lat - delta_lat
+            sidewalk_long = long - delta_long
+
+            print str(sidewalk_lat) + ',' + str(sidewalk_long)
 
     print
