a0cad69438cbdc72726c68dacc8a7077134d7445
Zachary Lawrence <zacharyclawrence@gmail.com>
2015-04-03 15:09:25 -0400

    Modified generator to generate its' own ids

--- generator.py
+++ generator.py
@@ -1,10 +1,23 @@
 class Generator(object):
-	output = list()
-	nodes = list()
-	ways = list()
-	bounds = None # minlat, minlon, maxlat, maxlon
+	output = []
+	nodes = []  # (id, long, lat)
+	ways = {}  # id -> ([node_ref], [(tag_key, tag_value)])
+	bounds = None  # minlat, minlon, maxlat, maxlon
+	id_gen = None  # Used to generate the next valid id (increments from 1)
 
-	def add_coord(self, id, long, lat):
+	def __init__(self):
+		self.id_gen = self.create_id()
+
+	def create_id(self):
+		count = 1
+
+		while True:
+			yield count
+			count += 1
+
+	def add_coord(self, long, lat):
+		id = self.id_gen.next()
+
 		if self.bounds is None:
 			self.bounds = (lat, long, lat, long)
 		else:
@@ -15,8 +28,17 @@ class Generator(object):
 
 		self.nodes.append((id, long, lat))
 
-	def add_way(self, id, node_ids, tags):
-		self.ways.append((id, node_ids, tags))
+		return id
+
+	def add_way(self, tags):
+		id = self.id_gen.next()
+
+		self.ways[id] = ([], tags)
+
+		return id
+
+	def add_way_reference(self, way_id, node_id):
+		self.ways[way_id][0].append(node_id)
 
 	def generate_header(self):
 		self.output.append('<?xml version="1.0" encoding="UTF-8"?>')
@@ -26,6 +48,9 @@ class Generator(object):
 			'" maxlat="' + str(self.bounds[2]) + \
 			'" maxlon="' + str(self.bounds[3]) + '"/>')
 
+	def generate_footer(self):
+		self.output.append('</osm>')
+
 	def generate_nodes(self):
 		for (id, long, lat) in self.nodes:
 			self.output.append(' <node id="' + str(id) + \
@@ -33,7 +58,9 @@ class Generator(object):
 				'" lon="' + str(long) +'"/>')
 
 	def generate_ways(self):
-		for (id, node_ids, tags) in self.ways:
+		for id in self.ways:
+			(node_ids, tags) = self.ways[id]
+
 			self.output.append(' <way id="' + str(id) + \
 				'" visible="true" user="test">')
 
@@ -51,4 +78,6 @@ class Generator(object):
 		self.generate_header()
 		self.generate_nodes()
 		self.generate_ways()
+		self.generate_footer()
+
 		return '\n'.join(self.output)
