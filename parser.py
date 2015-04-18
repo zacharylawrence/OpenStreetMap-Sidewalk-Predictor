38849a64ac8d0a3386df529a7865d918473629f0
Zachary Lawrence <zacharyclawrence@gmail.com>
2015-04-18 15:12:04 -0400

    Fixed accounting for end of road points (sliding window with points of less than 3)

--- parser.py
+++ parser.py
@@ -75,9 +75,42 @@ def window(seq, n=2):
 #         angle -= 2 * math.pi
 #     return angle
 
+def add_non_intersection(prev_id, id, next_id):
+    lat, long = counter.coords[id]
+
+    if prev_id is not None:
+        prev_lat, prev_long = counter.coords[prev_id]
+        in_angle = math.atan2(lat - prev_lat, long - prev_long)
+        average_angle = in_angle
+
+    if next_id is not None:
+        next_lat, next_long = counter.coords[next_id]
+        out_angle = math.atan2(next_lat - lat, next_long - long)
+        average_angle = out_angle
+
+    if prev_id is not None and next_id is not None:
+        average_x = (math.sin(in_angle) + math.sin(out_angle)) / 2
+        average_y = (math.cos(in_angle) + math.cos(out_angle)) / 2
+        average_angle = math.atan2(average_x, average_y)
+
+    perpendicular_angle = math.pi / 2 + average_angle
+
+    # Here we are assuming that things are on a flat plane, not mapped to a globe!
+    sidewalk_distance = 5 # In meters - Not currently used, just trial-and-errored 0.00005
+    delta_lat = math.sin(perpendicular_angle) * 0.00005
+    delta_long = math.cos(perpendicular_angle) * 0.00005
+
+    sidewalk_lat1 = lat + delta_lat
+    sidewalk_long1 = long + delta_long
+
+    sidewalk_lat2 = lat - delta_lat
+    sidewalk_long2 = long - delta_long
+
+    return (sidewalk_long1, sidewalk_lat1, sidewalk_long2, sidewalk_lat2)
+
 # instantiate counter and parser and start parsing
 if __name__ == "__main__":
-    log.basicConfig(format="", level=log.DEBUG)
+    # log.basicConfig(format="", level=log.DEBUG)
 
     counter = HighwayCounter()
     output = Generator()
@@ -98,41 +131,40 @@ if __name__ == "__main__":
     for osmid in counter.highways:                  # Way ids
         way1 = output.add_way([('highway', 'footway')])
         way2 = output.add_way([('highway', 'footway')])
+
+        # First Case:
+        if len(counter.highways[osmid]) > 1:
+            id = counter.highways[osmid][0]
+            next_id = counter.highways[osmid][1]
+
+            if id not in counter.intersections:
+                (sidewalk_long1, sidewalk_lat1, sidewalk_long2, sidewalk_lat2) = add_non_intersection(None, id, next_id)
+                output.add_way_reference(way1, output.add_coord(sidewalk_long1, sidewalk_lat1))
+                output.add_way_reference(way2, output.add_coord(sidewalk_long2, sidewalk_lat2))
+
+        # Middle Case:
         gen = window(counter.highways[osmid], 3)    # Coord ids (in order)
         for ids in gen:
             prev_id, id, next_id = ids
 
             if id not in counter.intersections:
-                prev_lat, prev_long = counter.coords[prev_id]
-                lat, long = counter.coords[id]
-                next_lat, next_long = counter.coords[next_id]
+                (sidewalk_long1, sidewalk_lat1, sidewalk_long2, sidewalk_lat2) = add_non_intersection(prev_id, id, next_id)
+                output.add_way_reference(way1, output.add_coord(sidewalk_long1, sidewalk_lat1))
+                output.add_way_reference(way2, output.add_coord(sidewalk_long2, sidewalk_lat2))
 
-                in_angle = math.atan2(lat - prev_lat, long - prev_long)
-                out_angle = math.atan2(next_lat - lat, next_long - long)
+        # Last Case:
+        if len(counter.highways[osmid]) > 1:
+            id = counter.highways[osmid][-1]
+            prev_id = counter.highways[osmid][-2]
 
-                average_x = (math.sin(in_angle) + math.sin(out_angle)) / 2
-                average_y = (math.cos(in_angle) + math.cos(out_angle)) / 2
-                average_angle = math.atan2(average_x, average_y)
+            if id not in counter.intersections:
+                (sidewalk_long1, sidewalk_lat1, sidewalk_long2, sidewalk_lat2) = add_non_intersection(prev_id, id, None)
+                output.add_way_reference(way1, output.add_coord(sidewalk_long1, sidewalk_lat1))
+                output.add_way_reference(way2, output.add_coord(sidewalk_long2, sidewalk_lat2))
 
-                perpendicular_angle = math.pi / 2 + average_angle
 
-                # Here we are assuming that things are on a flat plane, not mapped to a globe!
-                sidewalk_distance = 5 # In meters - Not currently used, just trial-and-errored 0.00005
-                delta_lat = math.sin(perpendicular_angle) * 0.00005
-                delta_long = math.cos(perpendicular_angle) * 0.00005
-
-                sidewalk_lat = lat + delta_lat
-                sidewalk_long = long + delta_long
-
-                log.debug(str(sidewalk_lat) + ',' + str(sidewalk_long))
-                output.add_way_reference(way1, output.add_coord(sidewalk_long, sidewalk_lat))
-
-                sidewalk_lat = lat - delta_lat
-                sidewalk_long = long - delta_long
-
-                log.debug(str(sidewalk_lat) + ',' + str(sidewalk_long))
-                output.add_way_reference(way2, output.add_coord(sidewalk_long, sidewalk_lat))
-
+        log.debug(str(sidewalk_lat1) + ',' + str(sidewalk_long1))
+        log.debug(str(sidewalk_lat2) + ',' + str(sidewalk_long2))
         log.debug('\n')
 
     print output.generate()
